5510affa088576992e39dc5a208b02f08bca6569
Priyanshu <43957309+priyanshunt@users.noreply.github.com>
2024-12-19 10:59:39 +0530

    Create Day 19

---
+++ 2024/d19q1.py
@@ -0,0 +1,26 @@
+d = None
+with open("d19q1.txt") as f:
+    d = f.read().split('\n')
+
+pattern = d[0].split(', ')
+design = d[2:]
+
+towel = {}
+def func(design):
+    if design in towel:
+        c = towel[design]
+    else:
+        c = 0
+        if design == "":
+            c = 1
+        else:
+            for x in pattern:
+                if design.startswith(x):
+                    c = c or func(design[len(x):])
+        towel[design] = c
+    return c
+
+c = 0
+for x in design:
+    c += func(x)
+print(c)
