301549897556014d1e5f79ac1c7f17e9cd591ed3
Priyanshu <43957309+priyanshunt@users.noreply.github.com>
2025-12-10 01:29:25 +0530

    Update d9q2.py

--- 2025/d9q2.py
+++ 2025/d9q2.py
@@ -1,29 +1,33 @@
 d = None
 with open("d9.txt", 'r') as f:
-    d = [list(map(int, reversed(x.strip().split(',')))) for x in f.readlines()]
+    d = [tuple(map(int, reversed(x.strip().split(','))))
+         for x in f.readlines()]
 
-d1 = [['.' for _ in range(14)] for _ in range(10)]
+d1 = []
+for i in range(len(d)):
+    x = d[i]
+    for j in range(i):
+        y = d[j]
+        d1.append([x, y, (abs(x[0] - y[0]) + 1) * (abs(x[1] - y[1]) + 1)])
 
-x1, y1 = d[-1]
-for z in d:
-    x2, y2 = z
-    print((x1, y1), (x2, y2))
-    if x1 == x2:
-        a, b = y1, y2
-        if a > b:
-            a, b = b, a
-        for z in range(a, b + 1):
-            d1[x1][z] = 'X'
-    else:
-        a, b = x1, x2
-        if a > b:
-            a, b = b, a
-        for z in range(a, b + 1):
-            d1[z][y1] = 'X'
-    x1, y1 = x2, y2
+d1.sort(key=lambda x: x[2], reverse=True)
 
-    for x in d1:
-        print(''.join(x))
+d2 = [(d[i-1], d[i]) for i in range(0, len(d))]
 
-# for x in d1:
-#     print(''.join(x))
+for x in d1:
+    x1, y1 = x[0]
+    x2, y2 = x[1]
+    c = True
+    for y in d2:
+        left = max(x1, x2) <= min(y[0][0], y[1][0])
+        right = min(x1, x2) >= max(y[0][0], y[1][0])
+        top = max(y1, y2) <= min(y[0][1], y[1][1])
+        bottom = min(y1, y2) >= max(y[0][1], y[1][1])
+
+        if not (left or right or top or bottom):
+            c = False
+            break
+
+    if c:
+        print(x[2])
+        break
