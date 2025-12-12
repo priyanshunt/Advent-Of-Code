0a6d658faf4e0687ae6ebccfbd26324ea22b9dea
Priyanshu <43957309+priyanshunt@users.noreply.github.com>
2025-12-12 14:12:22 +0530

    Update d10q2.py

--- 2025/d10q2.py
+++ 2025/d10q2.py
@@ -1,34 +1,30 @@
+from z3 import Solver, Int, Sum, sat
+
 bw = []
 jr = []
-with open("d9.txt", 'r') as f:
+with open("d10.txt", 'r') as f:
     d = [x.strip().split(' ') for x in f.readlines()]
     for x in d:
         bw.append([tuple(map(int, y[1:-1].split(','))) for y in x[1:-1]])
         jr.append(list(map(int, x[-1][1:-1].split(','))))
 
-s = {}
-l = None
-
-def rec(bw, jr, i, cost):
-    if jr.count(-1) > 0 or tuple(jr) in s and s[tuple(jr)] < cost:
-        pass
-    else:
-        s[tuple(jr)] = cost
-        if i < l:
-            jr1 = jr.copy()
-            for x in bw[i]:
-                jr1[x] -= 1
-            rec(bw, jr1, i, cost + 1)
-            rec(bw, jr.copy(), i + 1, cost)
-
 c = 0
-for i in range(len(bw)):
-    
-    s = {}
-    l = len(bw[i])
-    rec(sorted(bw[i], key = lambda x:len(x), reverse=True), jr[i], 0, 0)
-    
-    c += s[tuple([0 for _ in range(len(jr[i]))])]
-    print(i, c)
+for bwi, jri in zip(bw, jr):
+
+    s = Solver()
+    v = [Int(f"a{i}") for i in range(len(bwi))]
+    for v1 in v:
+        s.add(v1 >= 0)
+
+    for i, x in enumerate(jri):
+        v1 = [v[j] for j, b in enumerate(bwi) if i in b]
+        s.add(Sum(v1) == x)
+
+    while s.check() == sat:
+        model = s.model()
+        n = sum([model[d].as_long() for d in model])
+        s.add(Sum(v) < n)
+
+    c += n
 
 print(c)
